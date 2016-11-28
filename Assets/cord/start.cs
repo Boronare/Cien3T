e9e4a1ba33faabd824f1f38c9c49b39d4aab3b44
ghdtkazosel <ohyan19@gmail.com>
2016-11-28 20:54:47 +0900

    initialize

---
+++ Assets/cord/start.cs
@@ -0,0 +1,20 @@
+﻿
+	using UnityEngine;
+	using System.Collections;
+
+	public class First : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+			if(Input.GetKey(KeyCode.Return)) {
+				Application.LoadLevel ("joystick");
+			}
+			/* 엔터키를 누르면 main 신을 불러오라는 의미 */
+		}
+	}
