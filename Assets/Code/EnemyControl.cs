88b1ad91011fdeb54339d49af26e96fa20c042cc
rhqhddns-PC\rhqhddns <telesisks>
2016-12-21 23:25:43 +0900

    적 AI요소 추가(주인공이 시야 안에 들어갈 시 쫓아옴)

--- Assets/Code/EnemyControl.cs
+++ Assets/Code/EnemyControl.cs
@@ -5,24 +5,37 @@ public class EnemyControl : MonoBehaviour {
 	private float waitingtime = 2.0f;
 	private float wait;
 	private Vector2 baseposition;
+	private float chasingwill = 3.0f;
 	private float walkrange = 5.0f;
 	private Vector2 randomValue = Random.insideUnitCircle;
+	private Vector2 targetposition;
 	// Use this for initialization
 	void Start () {
 		baseposition = this.transform.position;
 		wait = waitingtime;
+		transform.Find ("Enemy Sight").position = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (wait > 0.0f) {
-			wait -= Time.deltaTime;
+		if (!Sight.PlayerFind) {
+			if (wait > 0.0f) {
+				wait -= Time.deltaTime*2;
+			} else {
+				wait -= Time.deltaTime*2;
+				transform.Translate (randomValue.x * Time.deltaTime / 2, randomValue.y * Time.deltaTime / 2, 0);
+				if (wait < -2.0f) {
+					wait = Random.Range (waitingtime, waitingtime * 2);
+					randomValue = Random.insideUnitCircle;
+				}
+			}
 		} else {
-			wait -= Time.deltaTime;
-			transform.Translate (randomValue.x * Time.deltaTime/2, randomValue.y * Time.deltaTime/2, 0);
-			if (wait < -2.0f) {
-				wait = Random.Range (waitingtime, waitingtime * 2);
-				randomValue = Random.insideUnitCircle;
+			targetposition = GameObject.Find ("주인공").transform.position;
+			chasingwill -= Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, targetposition, 5 * Time.deltaTime);
+			if (chasingwill < 0f) {
+				chasingwill = 3.0f;
+				Sight.PlayerFind = false;
 			}
 		}
 	}
---
+++ Assets/Code/Sight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sight : MonoBehaviour {
+	public static bool PlayerFind = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D c){
+		PlayerFind = true;
+	}
+}
