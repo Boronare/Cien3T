910639f377177be83b6cbb616c9ce6d5fba43b51
Kim Jeong Soo <KimJeongSoo@gimjeongsuui-MacBook.local>
2017-01-16 14:39:28 +0900

    신하고 캐릭터 선택완료. 스킬버튼 배치까지만 함.

--- Assets/Code/CharacterSelect.cs
+++ Assets/Code/CharacterSelect.cs
@@ -13,9 +13,10 @@ public class CharacterSelect : MonoBehaviour {
 
 	public void characterSelect(int character) {
 		transform.Find("SelectIndicator").position = transform.Find ("Character" + character).position;// 선택한 구간을 블러인 창을 두서 선택되었음을 표시한다 
-		Global.selectedChar = character;//캐릭터의 변수는 charater0, charater1.
+		Global.selectedChar = character;//캐릭터의 변수는 character0, character1.
 	}
 	public void NextScene(){
 		SceneManager.LoadScene ("Scene/main_scene_sw",LoadSceneMode.Single);//씬을 옮긴다.
 	}
+
 }
